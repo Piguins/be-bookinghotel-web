aae982f3c986c256df0895d366895b697438880e
kien <21522246@gm.uit.edu.vn>
2023-10-05 20:13:42 +0700

    update structure

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(assembly);
+        });
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        return services;
+    }
+}
